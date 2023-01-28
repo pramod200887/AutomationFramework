da4509d3e084a211df41c08f337b34e787036fec
pramod200887 <pramod200887@gmail.com>
2023-01-28 00:36:24 -0800

    packages and POM created

---
+++ src/test/java/com/framework/pageObjects/LoginPage.java
@@ -0,0 +1,14 @@
+package com.framework.pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+	WebDriver ldriver;
+	LoginPage(WebDriver rdriver)
+	{
+		ldriver=rdriver;
+		PageFactory.initElements(rdriver, this);
+	}
+}
